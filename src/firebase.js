dc5a9f00b5348e76790ac67bbb03b4110fe1980f
owentr1369 <tamvaa1306@gmail.com>
2022-05-19 22:28:43 +0700

    add form template

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+import "firebase/auth";
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyBxE2cHoHh3yTIKr2vS7khfw8_O2c2wobQ",
+  authDomain: "madam-lien-v2-authen.firebaseapp.com",
+  projectId: "madam-lien-v2-authen",
+  storageBucket: "madam-lien-v2-authen.appspot.com",
+  messagingSenderId: "851648944872",
+  appId: "1:851648944872:web:de7514a31f94e2335a075d",
+  measurementId: "G-C6QRN6CTTZ",
+});
+
+export const auth = app.auth();
+export default app;
