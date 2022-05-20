6c8e08942a25ee4a28f4bd0645d61d07a5d21cce
owentr1369 <tamvaa1306@gmail.com>
2022-05-20 11:21:11 +0700

    add react router and fix signup component

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,19 +2,21 @@ import React, { useRef, useState } from "react";
 import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "./context/AuthContext";
 
-const Signup = () => {
+export default function Signup() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
-  const { signup } = useAuth;
-  const [error, setError] = useState(``);
+  const { signup } = useAuth();
+  const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
   async function handleSubmit(e) {
     e.preventDefault();
+
     if (passwordRef.current.value !== passwordConfirmRef.current.value) {
       return setError("Passwords do not match");
     }
+
     try {
       setError("");
       setLoading(true);
@@ -22,34 +24,28 @@ const Signup = () => {
     } catch {
       setError("Failed to create an account");
     }
+
+    setLoading(false);
   }
 
   return (
-    <div>
+    <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-4">Sign up</h2>
+          <h2 className="text-center mb-4">Sign Up</h2>
           {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" required ref={emailRef}></Form.Control>
+              <Form.Control type="email" ref={emailRef} required />
             </Form.Group>
             <Form.Group id="password">
               <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                required
-                ref={passwordRef}
-              ></Form.Control>
+              <Form.Control type="password" ref={passwordRef} required />
             </Form.Group>
             <Form.Group id="password-confirm">
               <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control
-                type="password"
-                required
-                ref={passwordConfirmRef}
-              ></Form.Control>
+              <Form.Control type="password" ref={passwordConfirmRef} required />
             </Form.Group>
             <Button disabled={loading} className="w-100" type="submit">
               Sign Up
@@ -57,11 +53,9 @@ const Signup = () => {
           </Form>
         </Card.Body>
       </Card>
-      <div className="w100 text-center mt-2">
+      <div className="w-100 text-center mt-2">
         Already have an account? Log In
       </div>
-    </div>
+    </>
   );
-};
-
-export default Signup;
+}
